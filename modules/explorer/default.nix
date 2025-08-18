43b5dacbda181262b8871eb0da78c22bb6c7a6c3
Theo Dedeken <theo.dedeken@telenet.be>
2025-08-18 19:53:53 +0200

    make sure fd is available

--- modules/explorer/default.nix
+++ modules/explorer/default.nix
@@ -1,12 +1,13 @@
 {
-  lib,
+  pkgs,
   config,
   ...
 }: let
   inherit (config.nvix.mkKey) wKeyObj;
 in {
   imports = [./snacks];
-
+  # Snacks explorer requires this package for file searching
+  extraPackages = [pkgs.fd];
   wKeyList = [
     (wKeyObj [
       "<leader>e"
