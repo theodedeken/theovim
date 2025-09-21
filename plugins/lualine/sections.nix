6137937bcc53d43ce93e3c5f5b643262eeb648c0
Theo Dedeken <theo.dedeken@telenet.be>
2025-09-21 20:42:32 +0200

    Lualine tweaks

--- plugins/lualine/sections.nix
+++ plugins/lualine/sections.nix
@@ -1,9 +1,11 @@
-{ config, lib, ... }:
-let
+{
+  config,
+  lib,
+  ...
+}: let
   inherit (config.nvix) icons;
   inherit (lib.nixvim) mkRaw;
-in
-{
+in {
   plugins.lualine.settings.sections = {
     lualine_a = [
       {
@@ -16,7 +18,7 @@ in
         color = "SLGreen";
       }
     ];
-    lualine_b = [ "encoding" ];
+    lualine_b = ["encoding"];
     lualine_c = [
       {
         __unkeyed = "b:gitsigns_head";
@@ -27,18 +29,19 @@ in
         __unkeyed = "diff";
         source =
           mkRaw # lua
-            ''
-              (function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if vim.b.gitsigns_status_dict then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end)
-            '';
+          
+          ''
+            (function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if vim.b.gitsigns_status_dict then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end)
+          '';
         symbols = {
           added = mkRaw ''"${icons.git.LineAdded}" .. " " '';
           modified = mkRaw ''"${icons.git.LineModified}".. " "'';
@@ -74,48 +77,46 @@ in
         '';
         __unkeyed =
           mkRaw # lua
-            ''
-              function()
-                local ok, noice = pcall(require, "noice")
-                if not ok then
-                  return false
-                end
-                return noice.api.status.mode.get()
+          
+          ''
+            function()
+              local ok, noice = pcall(require, "noice")
+              if not ok then
+                return false
               end
-            '';
+              return noice.api.status.mode.get()
+            end
+          '';
       }
       {
         __unkeyed =
           mkRaw # lua
-            ''
-              function()
-                local clients = vim.lsp.get_clients()
-                local lsp_names = {}
-                if next(clients) == nil then
-                  return "Ls Inactive"
-                end
-                for _, client in ipairs(clients) do
-                  if client.name ~= "copilot" and client.name ~= "null-ls" and client.name ~= "typos_lsp" then
-                    local name = client.name:gsub("%[%d+%]", "") -- makes otter-ls[number] -> otter-ls
-                    table.insert(lsp_names, name)
-                  end
-                end
-
-                local formatters = require("conform").list_formatters()
-                local con_names = {}
-
-                for _, formatter in ipairs(formatters) do
-                  local name = formatter.name
-                  if formatter.available and (name ~= "squeeze_blanks" and name ~= "trim_whitespace" and name ~= "trim_newlines") then
-                    table.insert(con_names, formatter.name)
-                  end
-                end
-                local names = {}
-                vim.list_extend(names, lsp_names)
-                vim.list_extend(names, con_names)
-                return "[" .. table.concat(vim.fn.uniq(names), ", ") .. "]"
+          
+          ''
+            function()
+              local bufnr = vim.fn.bufnr()
+              -- Get clients attached to current buffer
+              local clients = vim.lsp.get_clients({bufnr = bufnr})
+              local lsp_names = {}
+              for _, client in ipairs(clients) do
+                  local name = client.name:gsub("%[%d+%]", "") -- makes otter-ls[number] -> otter-ls
+                  table.insert(lsp_names, name)
               end
-            '';
+
+              if next(lsp_names) == nil then
+                return "Ls Inactive"
+              end
+              return "[" .. table.concat(vim.fn.uniq(lsp_names), ", ") .. "]"
+            end
+          '';
+        on_click =
+          mkRaw #lua
+          
+          ''
+            function()
+              vim.cmd("LspInfo")
+            end
+          '';
       }
 
       {
@@ -129,32 +130,40 @@ in
       {
         __unkeyed =
           mkRaw # lua
-            ''
-              function()
-                if vim.g.disable_autoformat or vim.b.disable_autoformat then
-                  return "autoformat OFF"
-                end
-              end
-            '';
-        }
+          
+          ''
+            function()
+              return "autoformat OFF"
+            end
+          '';
+        cond =
+          mkRaw #lua
+          
+          ''
+            function()
+              return vim.g.disable_autoformat or vim.b.disable_autoformat
+            end
+          '';
+      }
     ];
-    lualine_y = [ "progress" ];
+    lualine_y = ["progress"];
     lualine_z = [
       "location"
       {
         __unkeyed =
           mkRaw # lua
-            ''
-              function()
-                local lsp_clients = vim.lsp.get_clients()
-                for _, client in ipairs(lsp_clients) do
-                  if client.name == "copilot" then
-                    return "%#SLGreen#" .. "${icons.kind.Copilot}"
-                  end
+          
+          ''
+            function()
+              local lsp_clients = vim.lsp.get_clients()
+              for _, client in ipairs(lsp_clients) do
+                if client.name == "copilot" then
+                  return "%#SLGreen#" .. "${icons.kind.Copilot}"
                 end
-                 return ""
               end
-            '';
+               return ""
+            end
+          '';
       }
     ];
   };
