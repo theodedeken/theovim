f5710390f5447e43b2f4baf7a8dc73fbf3db8156
Theo Dedeken <theo.dedeken@telenet.be>
2025-11-14 19:37:49 +0100

    tweak formatting

--- plugins/lsp/conform.nix
+++ plugins/lsp/conform.nix
@@ -30,7 +30,7 @@ in {
             if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
               return
             end
-            return { timeout_ms = 500, lsp_format = "fallback" }
+            return { timeout_ms = 500, lsp_format = "first" }
           end
         '';
     };
